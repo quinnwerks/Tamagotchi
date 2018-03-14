955c176dd464e1a7c933b61b20fceefff5d279a3
Quinn Smith <quinn.smith98@gmail.com>
2018-03-14 17:18:39 -0400

    added timer initalization in main. could move to a subroutine though.

--- main.s
+++ main.s
@@ -1,11 +1,38 @@
+.data
+.equ TIMER1, 0xFF202000
 .section .text
 .label _start
 _start:
+
+/* @ tiger is this format okay? I want to have nice indenting to make the
+   structure more clear. Up to debate tho. message me and lemme know.
+*/
     # initalize stack pointer
     movi sp, sp, 0x00FFFFFC
 
     # initalize devices
-    # initalize timer
+       # initalize timer 1
+       movia r8, TIMER1
+
+          # enable continue and interrupt r10 = (b11)
+          movi r10, 0x03
+          stwio r10, 4(r8)
+
+          # store time (r10 is now time = 1 second)
+          movui r10, 0xE100
+          stwio r10,  8(r8)
+          movui r10, 0x5F5
+          stwio r10, 12(r8)
+
+       # initalize keyboard interrput ????
+
+
+
+
+
+
+
+
     # start loop
     loop:
     br loop
